f3a3543f7c3e8f9451e7937aa12960209d154edb
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-10-20 11:48:30 +0200

    nu

--- js/Fio.js
+++ js/Fio.js
@@ -17,11 +17,7 @@ accordionItems.forEach(item => {
 
 
 //footer//
-document.getElementById('Fio').addEventListener('dblclick', function () {
-    const url = 'https://github.com/FiorellaDiGiulio'; // byt till önskad länk
-    const message = 'Hej! Besök min GitHub:\n' + url + '\n\nKlicka OK för att öppna länken i en ny flik.';
-    if (confirm(message)) {
-        window.open(url, '_blank', 'noopener');
-    }
-});
+document.getElementById("Fio").addEventListener("dblclick", () => {
+    alert("Fio Github: https://github.com/FiorellaDiGiulio");
+  });
  
